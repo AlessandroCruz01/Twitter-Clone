d8358793919b6cdf7c55c8b85886c7367276279c
Alessandro Cruz <62727627+AlessandroCruz01@users.noreply.github.com>
2021-07-14 14:03:45 -0500

    correcao de desc news

--- src/components/News/index.tsx
+++ src/components/News/index.tsx
@@ -5,7 +5,7 @@ import { Container } from './styles';
 const News: React.FC = () => {
   return(
       <Container>
-          <span> Assusntos do momento no Brasil</span>
+          <span> Assuntos do momento no Brasil</span>
           <strong>100% dos Brasileros são vacinados contra o Covid19</strong>
       </Container>
   );
